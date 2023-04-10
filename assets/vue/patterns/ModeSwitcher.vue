9b6723a35c5b41582d9e45c1050b3f2a5936bd5e
Lafllamme <teke900@gmail.com>
2023-04-10 19:49:33 +0200

    add dark and light mode option

--- assets/vue/patterns/ModeSwitcher.vue
+++ assets/vue/patterns/ModeSwitcher.vue
@@ -1,35 +1,40 @@
 <template>
   <div>
     <button @click="toggleTheme" type="button"
-            class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-      <svg :class="{'hidden': toggleDarkMode }" class=" w-5 h-5" fill="black"
-           viewBox="0 0 20 20"
-           xmlns="http://www.w3.org/2000/svg">
-        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-      </svg>
-      <svg :class="{'hidden' : toggleLightMode}" class=" w-5 h-5" fill="black"
-           viewBox="0 0 20 20"
-           xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-            fill-rule="evenodd" clip-rule="evenodd"></path>
-      </svg>
+            class="text-gray-500 dark:bg-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+
+      <component :is="themeIcon"/>
     </button>
   </div>
 </template>
 <script setup lang="ts">
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import LightmodeIcon from "./icons/svgs/lightmode-icon.vue";
+import DarkmodeIcon from "./icons/svgs/darkmode-icon.vue";
 
 const toggleDarkMode = ref<boolean>(false);
 const toggleLightMode = ref<boolean>(false);
+
+const themeIcon = computed(() => {
+  if (toggleDarkMode.value) {
+    return DarkmodeIcon;
+  } else if (toggleLightMode.value) {
+    return LightmodeIcon;
+  }
+});
 
 //change the icons inside the buttons based on previous settings
 if (localStorage.getItem('color-theme') === 'dark'
     || !localStorage.getItem('color-theme')
     && window.matchMedia('(prefers-color-scheme: dark').matches) {
-  toggleLightMode.value = true;
-} else {
   toggleDarkMode.value = true;
+} else {
+  toggleLightMode.value = true;
+}
+
+const toggleModes = () => {
+  toggleDarkMode.value = !toggleDarkMode.value;
+  toggleLightMode.value = !toggleLightMode.value;
 }
 
 /**
@@ -45,9 +50,11 @@ const toggleTheme = () => {
     if (localStorage.getItem('color-theme') === 'light') {
       document.documentElement.classList.add('dark');
       localStorage.setItem('color-theme', 'dark');
+      toggleModes();
     } else {
       document.documentElement.classList.remove('dark');
       localStorage.setItem('color-theme', 'light');
+      toggleModes();
     }
   }
 
@@ -61,5 +68,7 @@ const toggleTheme = () => {
       localStorage.setItem('color-theme', 'dark');
     }
   }
+  console.log(toggleLightMode.value, 'light mode');
+  console.log(toggleDarkMode.value, 'dark mode');
 }
 </script>
