6d45e41b9b93f2ff66d34998bbec9120e50f95ef
Lafllamme <teke900@gmail.com>
2023-04-16 19:40:03 +0200

    remove not needed import

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
 import BaseLayout from "./layout/BaseLayout.vue";
 import StickyBanner from "./patterns/StickyBanner.vue";
-import ModeSwitcher from "./patterns/ModeSwitcher.vue";
 
 </script>
 <template>
   <BaseLayout>
     <template #header>
       <h1>Header</h1>
-      <StickyBanner />
+      <StickyBanner/>
     </template>
     <template #main>
-      <h1>Main</h1>
     </template>
     <template #footer>
       <h1>Footer</h1>
     </template>
-    <slot />
+    <slot/>
   </BaseLayout>
 </template>
 <style lang="scss" scoped></style>
