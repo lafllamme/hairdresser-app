2af01536598c1809363e6c26393a1d2cd9ddc58f
Lafllamme <teke900@gmail.com>
2023-06-01 03:28:15 +0200

    add flowbite config
    For our component library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.html.twig', './assets/**/*.{vue,js,ts}'],
+  content: ['./templates/**/*.html.twig', './assets/**/*.{vue,js,ts}', './node_modules/flowbite/**/*.js'],
   darkMode: 'class',
   theme: {
     extend: {},
